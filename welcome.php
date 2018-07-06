d3e7d2d7d8a1e665e3ab9a8669c92f5a486568fd
Maaz Elahi <maazellahi123@gmail.com>
2018-07-06 14:50:35 +0500

    Welcome is changed

--- welcome.php
+++ welcome.php
@@ -1,8 +1,8 @@
 <html>
 <body>
+Random change<br>
 
 Welcome! <?php echo isset($_POST["cup"]) ? $_POST["cup"] : ''; ?><br>
 Your email address is : <?php echo isset($_POST["email"])? $_POST["email"] : ''; ?>
-
 </body>
 </html>
