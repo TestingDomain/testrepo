acd270aa6aca10f66c7b0dd6d87a284770d44699
Maaz Elahi <maazellahi123@gmail.com>
2018-07-10 16:52:53 +0500

    p2

--- welcome.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-Random change only for testing<br>
-
-Welcome! <?php echo isset($_POST["cup"]) ? $_POST["cup"] : ''; ?><br>
-Your email address is : <?php echo isset($_POST["email"])? $_POST["email"] : ''; ?>
-</body>
-</html>
